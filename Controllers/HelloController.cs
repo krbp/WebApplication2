a3cfca748c1ac2ec9888fbee644dc73931d9777c
krbp <165799825+krbp@users.noreply.github.com>
2024-05-23 02:38:12 +0200

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/Yell")]
+        public String YellHello()
+        {
+            return "HELLO";
+        }
+
+        [Route("/Say")]
+        public String SayHello()
+        {
+            return "Hello";
+        }
+    }
+}
